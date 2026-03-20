d4cc38a59819d75262647804c5222420b0e33a5b
mrunmayiiii <mrunmayik1920@gmail.com>
2026-03-20 21:49:45 +0530

    first commit

---
+++ Backend/src/main/java/CF_DuelProject/CF_DuelProject/CfDuelProjectApplication.java
@@ -0,0 +1,14 @@
+package CF_DuelProject.CF_DuelProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfDuelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfDuelProjectApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
