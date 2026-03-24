c5ee8cea9b6942ad86d2e10bbc4aead0d0f031db
TechDivs <divyanshkhatikar@gmail.com>
2026-03-24 20:23:19 +0530

    Search Engine completed

--- Backend/src/main/java/CF_DuelProject/CF_DuelProject/CfDuelProjectApplication.java
+++ Backend/src/main/java/CF_DuelProject/CF_DuelProject/CfDuelProjectApplication.java
@@ -8,7 +8,6 @@ public class CfDuelProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfDuelProjectApplication.class, args);
-		System.out.println("Hello World!");
 	}
 
 }
