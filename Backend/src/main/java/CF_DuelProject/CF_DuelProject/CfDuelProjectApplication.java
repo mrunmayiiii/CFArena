b21d0b7620ae7ae4ca99e27bf227354a7b2ee236
TechDivs <divyanshkhatikar@gmail.com>
2026-03-31 20:07:30 +0530

    added token bucket rate limiting

--- Backend/src/main/java/CF_DuelProject/CF_DuelProject/CfDuelProjectApplication.java
+++ Backend/src/main/java/CF_DuelProject/CF_DuelProject/CfDuelProjectApplication.java
@@ -9,7 +9,7 @@ public class CfDuelProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfDuelProjectApplication.class, args);
-		System.out.println("Hello World!");
+		// System.out.println("Hello World!");
 	}
 
 }
