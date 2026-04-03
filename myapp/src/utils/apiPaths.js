2bb95c7327667fc7ef33518608805f9865e5a1f5
mrunmayiiii <mrunmayik1920@gmail.com>
2026-04-03 17:44:26 +0530

    Frontend Deployment env variables

--- myapp/src/utils/apiPaths.js
+++ myapp/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_API_URL;
 //export const BASE_URL = 'http://localhost:8080'
 export const API_PATHS = {
   AUTH: {
