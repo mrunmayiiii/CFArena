a612590d0f724fd87bd9a4f4b75e0b48179dd9fe
mrunmayiiii <mrunmayik1920@gmail.com>
2026-04-03 14:33:35 +0530

    Vite base url for frontend deployment

--- myapp/src/utils/apiPaths.js
+++ myapp/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const BASE_URL = 'http://localhost:8080'
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+//export const BASE_URL = 'http://localhost:8080'
 export const API_PATHS = {
   AUTH: {
     REGISTER: "/auth/register",
